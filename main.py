37d750df5b89b25d33d3e236c73d331bfc606270
あいしぃー <61808728+aic-6301@users.noreply.github.com>
2022-10-28 17:39:48 +0900

    AFKほぼ完成()

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class Aicybot(commands.Bot):
         print('Loading any items')
         bot.admin_guild = bot.get_guild(1033496363897475163)
         bot.owner = bot.get_user(964887498436276305)
+        bot.afk_pass(os.getenv('afk-pass'))
         #prefix json loader
         try:
             with open('./data/prefix.json', encoding='UTF-8') as f:
